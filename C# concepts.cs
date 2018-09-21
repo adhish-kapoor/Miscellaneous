13960f7c1e1ebbf0366f05aa29931cf605a8ac7f
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-09-21 16:36:10 +0530

    Update C# concepts.cs

--- C# concepts.cs
+++ C# concepts.cs
@@ -1,7 +1,6 @@
-Char.IsNumber(Char) Method //method to check whether the specified Unicode character matches number or not
+Char.IsNumber(Char)  //method to check whether the specified Unicode character matches number or not
 
 using System;
- 
 class GFG {
  
     // Main Method
@@ -22,5 +21,31 @@ class GFG {
         char ch2 = 'c';
         result = Char.IsNumber(ch2);
         Console.WriteLine(result);    //False
+    }
+}
+
+Char.IsNumber(String, Int32) //method is used to check whether the specified string at specified position matches with any number or not.
+ 
+using System;
+class GFG {
+ 
+    // Main Method
+    static public void Main()
+    {
+ 
+        // Declaration of data type
+        bool result;
+ 
+        // checking for number in a
+        // string at a desired position
+        string str1 = "GeeksforGeeks";
+        result = Char.IsNumber(str1, 2);
+        Console.WriteLine(result);        //False
+ 
+        // checking for number in a
+        // string at a desired position
+        string str2 = "geeks5forgeeks";
+        result = Char.IsNumber(str2, 5);  //True
+        Console.WriteLine(result);
     }
 }
